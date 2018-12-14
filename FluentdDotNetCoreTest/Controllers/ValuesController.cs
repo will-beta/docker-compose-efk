d04865f61ae8757f60ebb016bdbf3355966351e1
wangweiming <wangweiming@gridsum.com>
2018-12-14 17:59:37 +0800

    add a project

---
+++ FluentdDotNetCoreTest/Controllers/ValuesController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+
+namespace FluentdDotNetCoreTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get([FromServices]ILogger<ValuesController> logger)
+        {
+            logger.LogInformation($"hey out info");
+            logger.LogWarning($"hey out warn");
+            logger.LogError($"hey in error");
+            using (var scope = logger.BeginScope($"scope{DateTimeOffset.Now.Second}"))
+            {
+                logger.LogInformation($"hey in info");
+                logger.LogWarning($"hey in warn");
+                logger.LogError($"hey in error");
+            }
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ FluentdDotNetCoreTest/Startup.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace FluentdDotNetCoreTest
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddLogging(b =>
+            {
+                b.AddConfiguration(Configuration.GetSection("Logging"));
+
+                var config = new LoggerConfiguration()
+                .ReadFrom.ConfigurationSection(Configuration.GetSection("Logging:Serilog"))
+                //.WriteTo.Fluentd(new Serilog.Sinks.Fluentd.FluentdSinkOptions("42.159.89.9", 24224)
+                //{
+                //    LingerEnabled = false,
+                //    NoDelay = true,
+                //    Tag = "FluentdDotNetCoreTest"
+                //})
+                ;
+                var logger = config.CreateLogger()
+                ;
+                b.AddSerilog(logger);
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory factory)
+        {
+            //app.UseExceptionless("vYAJulY8WNmU9v72gFspZkzp3S04yRpQnGdwrEUy");
+            //factory.AddExceptionless();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
